4a43069e9acbd06a2d40963d2b058174fd562b30
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2025-09-18 09:22:30 -0600

    feat: implemente open payments

---
+++ app/actions.ts
@@ -0,0 +1,179 @@
+'use server'
+
+import { createAuthenticatedClient, isFinalizedGrant } from "@interledger/open-payments"
+import { createHash } from "crypto"
+
+export type ActionState = {
+  error: string | null,
+  redirectTo?: string,
+}
+
+const CLIENT_WALLET_ADDRESS_URL = 'https://ilp.interledger-test.dev/op-test-mx-client'
+const KEY_ID = '7ba18321-0415-404b-90f9-a60eb1bfbd30'
+
+const SENDING_WALLET_ADDRESS_URL = 'https://ilp.interledger-test.dev/op-test-mx'
+const RECEIVING_WALLET_ADDRESS_URL = 'https://ilp.interledger-test.dev/op-test-mx'
+
+function verifyHash(
+  clientNonce: string,
+  interactNonce: string,
+  interactRef: string,
+  authServerUrl: string,
+  receivedHash: string
+) {
+  const data = `${clientNonce}\n${interactNonce}\n${interactRef}\n${authServerUrl}/`
+  const hash = createHash('sha-256').update(data).digest('base64')
+
+  return hash === receivedHash
+}
+
+export async function sendMoneyFirstStep(initialState: any, formData: FormData): Promise<ActionState> {
+
+  const opClient = await createAuthenticatedClient({
+    walletAddressUrl: CLIENT_WALLET_ADDRESS_URL,
+    keyId: KEY_ID,
+    privateKey: process.env.PRIVATE_KEY as string
+  })
+
+  const sendingWalletAddress = await opClient.walletAddress.get({
+    url: SENDING_WALLET_ADDRESS_URL,
+  })
+
+  const receivingWalletAddress = await opClient.walletAddress.get({
+    url: RECEIVING_WALLET_ADDRESS_URL,
+  })
+
+  const incomingPaymentGrant = await opClient.grant.request(
+    {
+      url: receivingWalletAddress.authServer,
+    },
+    {
+      access_token: {
+        access: [{
+          type: 'incoming-payment',
+          actions: ['read', 'complete', 'create'],
+        }]
+      }
+    }
+  )
+
+  if (!isFinalizedGrant(incomingPaymentGrant)) {
+    return {
+      error: 'Failed to get incoming payment grant'
+    }
+  }
+
+  const amount = formData.get('amount') as string
+
+  const incomingPayment = await opClient.incomingPayment.create(
+    {
+      url: receivingWalletAddress.resourceServer,
+      accessToken: incomingPaymentGrant.access_token.value,
+    },
+    {
+      walletAddress: receivingWalletAddress.id,
+      incomingAmount: {
+        assetCode: receivingWalletAddress.assetCode,
+        assetScale: receivingWalletAddress.assetScale,
+        value: '1000',
+      },
+    },
+  )
+
+  const quoteGrant = await opClient.grant.request(
+    {
+      url: sendingWalletAddress.authServer
+    },
+    {
+      access_token: {
+        access: [
+          {
+            type: 'quote',
+            actions: ['create', 'read']
+          }
+        ]
+      }
+    }
+  )
+
+  if (!isFinalizedGrant(quoteGrant)) {
+    return {
+      error: 'Failed to get quote grant'
+    }
+  }
+
+  const quote = await opClient.quote.create(
+    {
+      url: sendingWalletAddress.resourceServer,
+      accessToken: quoteGrant.access_token.value
+    },
+    {
+      walletAddress: sendingWalletAddress.id,
+      receiver: incomingPayment.id,
+      method: 'ilp'
+    }
+  )
+
+  console.log('\nStep 5: got quote on sending wallet address', quote)
+
+  return {
+    error: null
+  }
+
+}
+
+export async function sendMoneySecondStep(initialState: any, formData: FormData): Promise<ActionState> {
+  const opClient = await createAuthenticatedClient({
+    walletAddressUrl: CLIENT_WALLET_ADDRESS_URL,
+    keyId: KEY_ID,
+    privateKey: process.env.PRIVATE_KEY as string
+  })
+
+  const sendingWalletAddress = await opClient.walletAddress.get({
+    url: SENDING_WALLET_ADDRESS_URL,
+  })
+
+  const receivingWalletAddress = await opClient.walletAddress.get({
+    url: RECEIVING_WALLET_ADDRESS_URL,
+  })
+
+  const nonce = crypto.randomUUID()
+  const outgoingPaymentGrant = await opClient.grant.request(
+    {
+      url: sendingWalletAddress.authServer
+    },
+    {
+      access_token: {
+        access: [
+          {
+            type: 'outgoing-payment',
+            actions: ['read', 'create'],
+            /* limits: {
+              debitAmount: {
+                assetCode: quote.debitAmount.assetCode,
+                assetScale: quote.debitAmount.assetScale,
+                value: quote.debitAmount.value
+              }
+            }, */
+            identifier: sendingWalletAddress.id
+          }
+        ]
+      },
+      interact: {
+        start: ['redirect'],
+        finish: {
+          method: "redirect",
+          uri: `http://localhost:3004?paymentId=123`,
+          nonce: nonce,
+        },
+      }
+    }
+  )
+
+  console.log('\nStep 7: got outgoing payment grant on sending wallet address', outgoingPaymentGrant)
+
+  return {
+    error: null,
+    redirectTo: (outgoingPaymentGrant as any).interact.redirect
+  }
+}
